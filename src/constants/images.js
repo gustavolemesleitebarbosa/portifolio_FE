2da6a43d01e5b26e70d36977ca73750726e53cc6
GustavoLemesLB <gustavo.lemes@work.co>
2023-04-02 21:43:00 -0300

    plus one commit

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,23 +2,18 @@ import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import email from '../assets/email.png';
-import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mobile from '../assets/mobile.png';
-import mu5 from '../assets/mu5.png';
 import nextjs from '../assets/nextjs.png';
 import node from '../assets/node.png';
 import prisma from '../assets/prisma.png';
-import python from '../assets/python.png';
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -32,13 +27,8 @@ import circle_dark from '../assets/circle_dark.svg';
 import logo from '../assets/logo.png';
 import profile from '../assets/profile.png';
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
 import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
 
 import RN from '../assets/RN.png';
 
@@ -48,20 +38,15 @@ export default {
   api,
   cpp,
   css,
-  figma,
-  flutter,
   git,
   graphql,
   html,
   javascript,
-  mu5,
   node,
-  python,
   react,
   redux,
   sass,
   typescript,
-  vue,
   about01,
   about02,
   about03,
@@ -70,13 +55,8 @@ export default {
   circle,
   circle_dark,
   logo,
-  adidas,
-  amazon,
-  asus,
   bolt,
   nb,
-  skype,
-  spotify,
   nextjs,
   android,
   nextjsNeg, 
